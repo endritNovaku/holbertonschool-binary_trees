75c528187f519745caa22b4f8654115e33f19467
endritNovaku <ennald@outlook.com>
2022-07-18 05:16:52 -0700

    adding task 0 and 1

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a binary tree
+ * @parent: pointer to the parent node
+ * @value: value to put in the new node
+ * Return: a pointer to the new node or NULL if it fails
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmpNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+
+	tmpNode->n = value;
+	tmpNode->parent = parent;
+	tmpNode->left = NULL;
+	tmpNode->right = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	parent = tmpNode;
+	return (parent);
+}
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert node as the left child of another node
+ * @parent: parent node
+ * @value: value of the new node
+ * Return: a pointer to the new node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmpNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+
+	tmpNode->n = value;
+	tmpNode->parent = parent;
+	tmpNode->left = NULL;
+	tmpNode->right = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		tmpNode->left = parent->left;
+		parent->left->parent = tmpNode;
+	}
+	parent->left = tmpNode;
+	return (parent);
+}
