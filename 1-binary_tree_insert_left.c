ed373b152a6f8bc3468e0c0cab5193b15a54ca93
endritNovaku <ennald@outlook.com>
2022-07-20 01:58:54 -0700

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tmpNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
-	if (tmpNode == NULL || value == NULL)
+	if (tmpNode == NULL)
 		return (NULL);
 
 	tmpNode->n = value;
