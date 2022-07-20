f27586c4b251256aa7ff2836df7055da9939df3a
endritNovaku <ennald@outlook.com>
2022-07-20 01:34:48 -0700

    fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -24,5 +24,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	total = node1 + node2;
 
-	return(total);
+	return (total);
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,20 +8,15 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree)
 	{
+                printf("%i, ", tree->n);
 		binary_tree_is_full(tree->left);
 		binary_tree_is_full(tree->right);
-		if (tree->left == NULL && tree->right != NULL)
-			return	(0);
-		else if (tree->left != NULL && tree->right == NULL)
-                        return  (0);
-		printf("%i, ", tree->n);
 	}
 
-	return	(1);
+	return (1);
 }
